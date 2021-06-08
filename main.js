f2f25dd5d29d3f98c47a259d6e4ce5591c8b8d4b
maxim1976 <59217582+maxim1976@users.noreply.github.com>
2021-06-08 19:14:36 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+const app = Vue.createApp({
+    data: function() {
+        return {
+            product: 'Socks',
+            descr: 'Amazing, lovely socks!',
+            image: './assets/images/socks_green.jpg'      
+        }
+    }
+})
